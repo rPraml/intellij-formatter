283ec1c06291ff25cf1b7991c8abef523d22116b
Roland Praml <roland.praml@foconis.de>
2024-02-27 14:21:30 +0100

    Ultimate formatter works

---
+++ intellij-ultimate/format.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ------------------------------------------------------
+# IntelliJ IDEA formatting script.
+# ------------------------------------------------------
+
+echo "Using modified format.sh"
+
+groupadd -g "$UID" "$UNAME"
+useradd -u "$UID" -g "$UID" --no-create-home -d /tmp "$UNAME"
+
+su "$UNAME" -c "mkdir -p /tmp/ic/config"
+su "$UNAME" -c "cp /tmp/idea.key /tmp/ic/config"
+su "$UNAME" -c id
+
+IDE_BIN_HOME="${0%/*}"
+su "$UNAME" -c "$IDE_BIN_HOME/idea.sh"\ format\ "$*"
+
